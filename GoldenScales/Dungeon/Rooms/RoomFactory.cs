7b6bdd1f1c5853af41e573c3be7766e2d86fbd21
Martin Work <martin.kloeckener@gmail.com>
2023-02-09 17:52:56 +0100

    renamed to GoldenScales

--- GoldenScales/Dungeon/Rooms/RoomFactory.cs
+++ GoldenScales/Dungeon/Rooms/RoomFactory.cs
@@ -10,37 +10,26 @@ public static class RoomFactory
         {
             case RoomType.NONE:
                 throw new Exception("Please specify a room type.");
-                break;
             case RoomType.StartRoom:
                 return new StartRoom(roomPosition);
-                break;
             case RoomType.EmptyRoom:
                 return new EmptyRoom(roomPosition);
-                break;
             case RoomType.EnemyRoom:
                 return new EnemyRoom(roomPosition);
-                break;
             case RoomType.TraderRoom:
                 return new TraderRoom(roomPosition);
-                break;
             case RoomType.TreasureRoom:
                 return new TreasureRoom(roomPosition);
-                break;
             case RoomType.ChestRoom:
                 return new ChestRoom(roomPosition);
-                break;
             case RoomType.MimicRoom:
                 return new MimicRoom(roomPosition);
-                break;
             case RoomType.BossRoom:
                 return new BossRoom(roomPosition);
-                break;
             case RoomType.TrapRoom:
                 return new TrapRoom(roomPosition);
-                break;
             case RoomType.HealingWellRoom:
                 return new HealingWellRoom(roomPosition);
-                break;
             default:
                 throw new ArgumentOutOfRangeException(nameof(roomType), roomType, null);
         }
